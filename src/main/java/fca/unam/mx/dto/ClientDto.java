de937820751086719197d35679e729744e1f527a
jlescalona <76709908+jlescalona@users.noreply.github.com>
2020-12-28 16:53:45 -0600

    Create ClientDto.java

---
+++ src/main/java/fca/unam/mx/dto/ClientDto.java
@@ -0,0 +1,58 @@
+package fca.unam.mx.dto;
+
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
+
+public class ClientDto {
+    private Long id;
+    private String name;
+    private String last_name;
+    private String second_last_name;
+    private String RFC;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ColumnName("nombre")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @ColumnName("apellido_paterno")
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    @ColumnName("second_last_name")
+    public String getSecond_last_name() {
+        return this.second_last_name;
+    }
+
+    public void setSecond_last_name(String second_last_name) {
+        this.second_last_name = second_last_name;
+    }
+
+    @ColumnName("RFC")
+     public String getRFC() {
+        return RFC;
+    }
+
+    public void setRFC(String RFC) {
+        this.RFC = RFC;
+    }
+
+    
+
+}
